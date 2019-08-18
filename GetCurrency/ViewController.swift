029708d1be3a85cfad46bef3ce46cd07321e7fd2
Kouki Saito <dan.addr.skd@gmail.com>
2019-08-18 18:40:09 +0900

    Initial commit

---
+++ GetCurrency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetCurrency
+//
+//  Created by Kouki Saito on 8/18/19.
+//  Copyright © 2019 Kouki Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
